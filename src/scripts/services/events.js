6ea831365bd68ef7e460827679e3bad48f77db28
MATEUS RISSO DI BLASIO <139410293+MateusRissoDiBlasio@users.noreply.github.com>
2024-05-28 09:23:12 -0300

    Update events.js

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,8 +1,8 @@
-import { baseUrl, eventsQuantity } from '/src/scripts/variables.js'
+import { baseUrl, eventsQuantity } from '/src/scripts/variables.js';
 
 async function getEvents(userName){
    
-    const response = await fetch(`${baseUrl}${userName}/events`)
+    const response = await fetch(`${baseUrl}${userName}/events`);
     const events = await response.json();
     return events.filter(element => element.type === 'CreateEvent' || element.type === 'PushEvent').slice(0,eventsQuantity);
 }
